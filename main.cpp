bd004d81ddbb0d980d31af3616fa56678e4c5b47
KamronUrinboyev <250232@newuu.uz>
2025-09-11 11:59:57 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,101 @@
+//
+// #include <iostream>
+// using namespace std;
+
+//Problem 1
+// int main() {
+//     int intNumber = 21; //number of students in the lab
+//     float floatNumber = 3.14; // PI value
+//     double doubleNumber = 45.1234; // double variable
+//     bool boolean = true; // bool variable
+//     char charName = 'A'; // char variable
+//
+//      cout << "intNumber = " << intNumber
+//      << " | Size: " << sizeof(intNumber) << " bytes" << endl;
+//
+//     cout << "floatNumber = " << floatNumber
+//          << " | Size: " << sizeof(floatNumber) << " bytes" << endl;
+//
+//     cout << "doubleNumber = " << doubleNumber
+//          << " | Size: " << sizeof(doubleNumber) << " bytes" << endl;
+//
+//     cout << "boolean = " << boolean
+//          << " | Size: " << sizeof(boolean) << " bytes" << endl;
+//
+//     cout << "charName = " << charName
+//          << " | Size: " << sizeof(charName) << " bytes" << endl;
+//
+//     return 0;
+// }
+
+//problem 2
+// int main() {
+//         // Printing personal information
+//         string name = "Kamron Urinboyev"
+//         int id = 250232
+//         string music = "Tony Ann - ICARUS"
+//         string movie = "Oppenheimer"
+//         cout << name << endl;
+//         cout << id << endl;
+//         cout << music << endl;
+//         cout << movie << endl;
+//
+//         return 0;
+// }
+
+//problem 3
+// int main() {
+//
+//     cout << "\"I\'m standing\" on the edge of some crazy cliff." << endl;
+//     cout << "What I have to do, I have to catch everybody\\" << endl;
+//     cout << " if they start to go over the cliff-I mean\\" << endl;
+//     cout << "  \\if they're running and they don't look" << endl;
+//     cout << "  where they're going I have to come out" << endl;
+//     cout << " from somewhere and    catch them." << endl;
+//         return 0;
+//     }
+//
+
+
+//problem 4
+// int main() {
+//     int a = 189;
+//     char b = 'B';
+//     float res1 = a / 3.0;
+//     int res2 = b;
+//     float res3 = (a + b) / 5.0;
+//     char res4 = b + ((a/10)-1);       // move forward in ASCII table
+//
+//     // Step 3: Print results in one row
+//     cout << res1 << " " << res2 << " " << res3 << " " << res4 << endl;
+//
+//     return 0;
+// }
+
+
+//problem 5
+// int main() {
+//     int a = 12;
+//     int b = 38;
+//     int sum = a + b;
+//     int prod = a * b;
+//     cout << "Sum = " << sum << endl;
+//     cout << "Prod = " << prod << endl;
+//     return 0;
+//
+// }
+
+//problem 6
+// int main() {
+//     int a;
+//     int b;
+//     cout << "Enter the first number: ";
+//     cin >> a;
+//     cout << "Enter the second number: ";
+//     cin >> b;
+//     int sum = a + b;
+//     cout << "Sum: " << a << " + " << b << " = " << sum << endl;
+//
+//     return 0;
+// }
+
